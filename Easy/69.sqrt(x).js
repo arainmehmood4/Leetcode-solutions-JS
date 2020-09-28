02077bf08779a78d31144a3d29a849d647faf1df
arainmehmood4 <69026330+arainmehmood4@users.noreply.github.com>
2020-09-28 03:50:32 -0700

    Update 69.sqrt(x).js

--- Easy/69.sqrt(x).js
+++ Easy/69.sqrt(x).js
@@ -2,37 +2,20 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) 
-{
-	if (x === 1 || x === 0) 
-    {
-		return x;
-	} 
-    else 
-        if (x < 0) 
-        {
-            return 0;
+var mySqrt = function(x) {
+    let L=0, R=x;
+    while(L <= R){
+        let mid= parseInt(L+(R-L)/2);
+        if(mid * mid===x){
+            return mid;
         }
-
-	var min = 1, max = x+1;
-	while (true) 
-    {
-		let mid = min + Number.parseInt((max-min)/2);
-
-		if (mid*mid > x) 
-        {
-			max = mid-1;
-		} 
-        else 
-        {
-			if ((mid+1)*(mid+1) > x) 
-            {
-				return mid;
-			} 
-            else 
-            {
-				min = mid+1;
-			}
-		}
+        else if(mid * mid < x){
+            L=mid+1;
+            ans=mid;
+        }
+        else {
+            R=mid-1;
+        }
     }
+    return ans;
 };
