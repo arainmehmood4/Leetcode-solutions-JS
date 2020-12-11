8ef1d15c432d55529abeeb135344291bd329e67a
arainmehmood4 <69026330+arainmehmood4@users.noreply.github.com>
2020-12-11 07:01:44 +0500

    working commit - Created using LeetHub

---
+++ sqrtx/sqrtx.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let L = 0, R = x;
+    while(L <= R) {
+        let mid= parseInt(L+(R-L)/2);
+        if(mid * mid == x) {
+            return mid;
+        }
+        else if(mid * mid < x){
+            L = mid + 1;
+            ans = mid;
+        }
+        else {
+            R = mid - 1;
+        }
+    }
+    return ans;
+};
