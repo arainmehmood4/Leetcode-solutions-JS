500b9ec430283210358b42e1af0a64e730044d3f
arainmehmood4 <69026330+arainmehmood4@users.noreply.github.com>
2020-12-18 16:59:26 +0500

    Time: 228 ms (73.29%), Space: 0B (100.00%) - LeetHub

--- big-countries/big-countries.sql
+++ big-countries/big-countries.sql
@@ -1,4 +1,7 @@
-# Write your MySQL query statement below
-SELECT name , population, area
+select name , population, area 
 from world
-where population > 25000000 OR area > 3000000 ;
+where population > 25000000 OR area > 3000000;
+# union
+# select name , population, area
+# from world
+# where 
